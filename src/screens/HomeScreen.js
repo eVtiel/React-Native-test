451d3294af321cc08bfd0afc15c91f249038cff4
Alex Shapovalov <evtiel.go@gmail.com>
2020-08-06 02:39:43 -0400

    How navigation button works

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
 
-const HomeScreen = () => {
+const HomeScreen = (props) => {
   return (
     <View>
       <Text style={styles.text}>Hi There</Text>
       <Button 
         title="Go To Components Demo"
-        onPress={() => console.log('Button pressed')}
+        onPress={() => props.navigation.navigate('Components')}
       />
       <TouchableOpacity onPress={() => console.log('List Pressed')}>
         <Text>Go To List Demo</Text>
