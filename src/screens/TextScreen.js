2152c8541a9399486aee24efba7553b7d8891568
Alex Shapovalov <evtiel.go@gmail.com>
2020-08-07 13:09:39 -0400

    using reducer

---
+++ src/screens/TextScreen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text, View, Input, StyleSheet} from 'react-native';
+
+const TextScreen = () => {
+    return <Text>some</Text>;
+}
+
+const styles = StyleSheet.create({});
+
+export default TextScreen;
